6fb01a8baa5c1c537b85b71ec5633fa0a3b5b627
James Carruthers <carruthers.j@etoncollege.org.uk>
2023-02-04 11:10:22 +0000

    beginning of comment card writer

--- CommentCardWriter/ContentView.swift
+++ CommentCardWriter/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showComment: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            if showComment {
+                Text("Comment is here")
+            } else {
+                Text("")
+            }
+            Button("Show comment") {
+                toggleShow()
+            }
+            .foregroundColor(.green)
+        }
+    }
+    
+    func toggleShow() {
+        showComment.toggle()
     }
 }
 
---
+++ Model/Comments.swift
@@ -0,0 +1,24 @@
+//
+//  Comments.swift
+//  CommentCardWriter
+//
+//  Created by Carruthers, James (EJNR) on 04/02/2023.
+//
+
+import Foundation
+
+class Comments {
+    var comments = [String]()
+    
+    init(comments: [String]) {
+        comments = comments
+    }
+    
+    func randomComment() {
+        for
+    }
+    
+    #if DEBUG
+    let exampleComments = Comments(comments: ["Comment 1", "Comment 2", "Comment 3"])
+    #endif
+}
